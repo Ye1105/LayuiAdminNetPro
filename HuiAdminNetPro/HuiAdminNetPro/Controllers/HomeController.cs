f656346eb8d9e354efbdd6bf448e21c390b8a03c
15868707168@163.com <15868707168@163.com>
2023-05-08 17:55:25 +0800

    代码清理

--- HuiAdminNetPro/HuiAdminNetPro/Controllers/HomeController.cs
+++ HuiAdminNetPro/HuiAdminNetPro/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using HuiAdminNetPro.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace HuiAdminNetPro.Controllers
 {
@@ -16,17 +14,6 @@ namespace HuiAdminNetPro.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
